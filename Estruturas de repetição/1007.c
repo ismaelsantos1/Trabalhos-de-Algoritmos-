1250d80b31174ba3026e101be2cc14fc76d4e377
Ismael dos Santos <ismaelshenrique11@gmail.com>
2023-07-10 11:23:47 -0300

    hmm

---
+++ Estruturas de repetição/1007.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*int main(){
+
+    int i=1;
+
+    do{
+        printf("Iteração %d\n", i);
+        i +=1; //Vai imprimir somente os números pares
+        //i +=2; //para imprimir os números ímpares
+    } while (i<=5); //i >=1 tornaria um loop infinito
+
+    return 0;
+    
+}*/
+
+int main(){
+
+    int x;
+
+    do{
+        printf("Digite um número maior que z");
+        scanf("%d", &x);
+
+
+    }while(x<=0);
+        
+    printf("\n\nX = %d", x);
+
+    return 0;
+}
