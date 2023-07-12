312343601740d6eb2b9f7fbd59925561aa362e45
Ismael dos Santos <ismaelshenrique11@gmail.com>
2023-07-12 11:43:49 -0300

    salvando

--- Estruturas de repetição/Exercicios da aula/2.c
+++ Estruturas de repetição/Exercicios da aula/2.c
@@ -10,21 +10,65 @@ c) a média salarial por categoria de gênero;
 
 int main(){
 
-    int idade, cont=1, genero;
-    float salario;
+    int idade, cont=1, genero, salTotal=0, tPessoas;
+    float salario, mSalario, maiorIdade, mSalPCat;
 
     printf("A prefeitura está realizando uma pesquisa com a população");
 
     do
     {
-        printf("Digite sua idade: ");
-        scanf("%d", &idade);
+        printf("Informe seu genêro: \n1. Masculino \n2. Feminino \n3. Outro \n 4. Não informado");
+        scanf("%d", &genero);
+        switch (genero)
+        {
+        case 1:
+            printf("Digite sua idade: ");
+            scanf("%d", &idade);
 
-        printf("\n\nDigite seu gênero: ");
-        scanf("%", &genero);
+            printf("Digite seu salário: ");
+            scanf("%f", &salario);
+
+            break;
+        
+        case 2: 
+
+            printf("Digite sua idade: ");
+            scanf("%d", &idade);
+
+            printf("Digite seu salário: ");
+            scanf("%f", &salario);
+            break;
+
+        case 3:
+            printf("Digite sua idade: ");
+            scanf("%d", &idade);
+
+            printf("Digite seu salário: ");
+            scanf("%f", &salario);
+
+        case 4: 
+            printf("Digite sua idade: ");
+            scanf("%d", &idade);
+
+            printf("Digite seu salário: ");
+            scanf("%f", &salario);
+
+        default:
+            printf("Digite um valor válido");
+            break;
+
+        salTotal += salario; //Armazenando os valores dos salários informados
+        tPessoas += genero;
+        }
+        if(salario > 0 ){
+            //Tirando a média salarial de todos juntos
+            mSalario = salTotal / tPessoas;
+
+        }
+        
 
     
 
-    } while (/* condition */);
+    } while;
     
 }
