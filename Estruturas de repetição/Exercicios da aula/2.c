545709a532676bf874907c991d65a316f25f2eaf
Ismael dos Santos <ismaelshenrique11@gmail.com>
2023-07-12 12:42:04 -0300

    salvando2

--- Estruturas de repetição/Exercicios da aula/2.c
+++ Estruturas de repetição/Exercicios da aula/2.c
@@ -10,14 +10,14 @@ c) a média salarial por categoria de gênero;
 
 int main(){
 
-    int idade, cont=1, genero, salTotal=0, tPessoas, maiorIdade, menorIdade, gMasc=0, gFem=0, gOutro=0, gNinfo=0;
+    int idade, cont=1, genero, salTotal=0, tPessoas, maiorIdade=0, menorIdade=122, gMasc=0, gFem=0, gOutro=0, gNinfo=0;
     float salario, mSalario, mSalMasc=0, mSalFem=0, mSalOutr=0, mSalNinfo=0;
 
     printf("A prefeitura está realizando uma pesquisa com a população");
 
     do
     {
-        printf("Informe seu genêro: \n1. Masculino \n2. Feminino \n3. Outro \n 4. Não informado");
+        printf("\n\nInforme seu genêro: \n1. Masculino \n2. Feminino \n3. Outro \n4. Não informado\n");
         scanf("%d", &genero);
         switch (genero)
         {
@@ -74,12 +74,10 @@ int main(){
         tPessoas += genero;
 
         }
-        if(salario > 0 && idade > 0){
+        if(salario > 0 && idade > maiorIdade){
             //Tirando a média salarial de todos juntos
             mSalario = salTotal / tPessoas;
             idade = maiorIdade;
-            idade = menorIdade;
-            
 
             if(genero = 1){
                 mSalario = mSalMasc / gMasc;
@@ -87,11 +85,20 @@ int main(){
             if(genero = 2){
                 mSalario = mSalFem / gFem;
             }
+            if(genero = 3){
+                mSalNinfo = mSalNinfo / gNinfo;
+            }
+            if(genero = 4){
+                mSalOutr = mSalOutr / gOutro;
+            }
+        } else if(salario > 0 && idade < menorIdade){
+            idade = menorIdade;
         }
-        
-
     
 
-    } while(cont<=5);
+    } while(cont<=5); //aqui eu controlo quantas vezes a função irá rodar
     
+    printf("O resultado da pesquisa é: \n\n%.2f \n\n%d \n\n%d \n\n%.2f \n\n%.2f \n\n%.2f \n\n%.2f", mSalario, maiorIdade, menorIdade, mSalMasc, mSalFem, mSalNinfo, mSalOutr);
+
+    return 0;
 }
