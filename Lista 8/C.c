081a07e93c338bc861bcc9b2c41664849cdf9efd
Ismael dos Santos <ismaelshenrique11@gmail.com>
2023-07-24 23:30:45 -0300

    Letra D lista 8 finalizado

--- Lista 8/C.c
+++ Lista 8/C.c
@@ -9,3 +9,35 @@ iv) O valor da primeira prestação das compras a prazo, sabendo-se que essas
 serão pagas em três vezes
 */
 
+#include <stdio.h>
+
+    int main(){
+
+        int cod;
+        float comprasP=0, comprasV=0, sum; 
+    
+        printf("Formas de pagamento disponíveis: \n\nÀ vista: 1\n\nA prazo: 2\n\nFinalizar a operação: 0\n\n");
+
+        for(cod=1; cod>0; sum=0){
+            printf("Digite a forma de pagamento: ");
+            scanf("%d", &cod);
+                switch(cod){
+                    case 1:
+                        printf("Digite o valor da transação: ");
+                        scanf("%f", &sum);
+                            if(sum > 0)
+                                comprasV += sum;
+                        break;
+
+                    case 2:
+                        printf("Digite o valor da transação: ");
+                        scanf("%f", &sum);
+                            if(sum > 0)
+                                comprasP += sum;
+                        break;
+            }
+        }
+        printf("Esse é o total das compras: \n\nÀ vista: R$%.2f\n\nA prazo: R$%.2f\n\nTotal de compras: %.2f\n\nValor da primeira prestação em 3x: R$%.2f\n\n\nObrigado por comprar conosco. Volte Sempre! <3", comprasV, comprasP, (comprasP + comprasV), (comprasP/3));
+
+        return 0;
+    }
