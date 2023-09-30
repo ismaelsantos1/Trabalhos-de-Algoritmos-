587038a9f14452301d33e6f706935881f4dddeac
Ismael dos Santos <ismaelshenrique11@gmail.com>
2023-09-30 17:03:46 -0300

    up1

---
+++ Dia29092023.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    //int tamanho;
+    //char n[10];
+    //char vogais [] = {'a', 'b', 'c'}; // vai armazenar valores dentro da string, para isso usa só aspas simples. Quando for nome ou algo "complexo" deve ser usado aspas duplas
+
+
+        //printf("Digite um nome: ");
+        //fgets(n, 10, stdin); /*stdin é o dispositivo de entrada padrão*/
+        //scanf("%[^\n]", n);
+        //printf("%s", n);
+        //puts(n); // ele mostra o que foi armazenado e dá uma quebra de linha
+        //puts ("Tchau!"); //funciona como um printf, mas dá uma quebra de linha e não permite pôr uma variável junto
+        //tamanho = strlen(n); //strlen vai mostrar o tamanho da string, ou seja, a quantidade de caracteres que tem nela
+        //fflush limpa o buffer para não pegar valores já armazenados
+
+        /*A Função strcmp retorna uma comparação de daus strings, recebe duas strings como argumento e retorna um valor inteiro
+
+            < 0, se string1 < string2
+            = 0, se string1 = string2
+            > 0, se string1 > string2
+        
+            Como usar: strcmp(string1, string2);
+
+            Ele usa como critério a ordem alfabética para comparar e só para quando encontra um diferente ou, se não encontrar, ele para quando chegar no final, assim ele irá retornar 0. Se estiver na ordem alfabética, ele irá retornar um valor negativo.
+        */
+
+
+        //printf("Tamanho da string é: %d", tamanho);
+    
+    
+        char resp[40] = "branco";
+        char r1[40];
+
+            printf("Qual a cor do cavalo branco de Napoleão? \n");
+            fgets(r1, 40, stdin);
+
+            if(strcmp(r1, resp) != 0){
+            
+                //while(strcmp(r1, resp) != 0){
+                
+                    printf("Resposta errada! Tente novamente: ");
+                    fgets(r1,40,stdin);
+                //}
+
+            }else{
+                
+                printf("Resposta correta!\n");
+                
+            }
+        
+        
+    return 0;
+
+}
