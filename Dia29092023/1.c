ea83d6f7e69d55d5ec58890490015c6e93966dc5
Ismael dos Santos <ismaelshenrique11@gmail.com>
2023-10-04 02:10:50 -0300

    código do primeiro exercício rodando quase 99%

--- Dia29092023/1.c
+++ Dia29092023/1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*    1. Criar um programa que receba 3 nomes e apresente-os em ordem alfabética*/
     
     int main(){
 
-        char nome1[26], nome2[26], nome3[26], i;
-        int  comp1, comp2, comp3;
+        char nome1[26], nome2[26], nome3[26], aux[26];
+        //int  comp1, comp2, comp3;
 
         printf("Digite um primeiro nome: ");
         fgets(nome1, 26, stdin);
@@ -17,31 +18,44 @@
         printf("Digite um terceiro nome: ");
         fgets(nome3, 26, stdin);
 
-            comp1 = strcmp(nome1, nome2);
+            //Opção 1 pra rodar
+            if(strcmp(nome1, nome2) > 0 && strcmp(nome1, nome3)>0){
+
+                strcpy(aux, nome3);
+                strcpy(nome3, nome2);
+                puts(aux);
+                puts(nome3);
+                puts(nome1);
+
+            } else if(strcmp(nome2, nome1)>0 && strcmp(nome2, nome3)>0){
+
+                strcpy(aux, nome2);
+                strcpy(nome2, nome1);
+                puts(nome3);
+                puts(nome1);
+                puts(aux);
+
+            } else if(strcmp(nome3, nome1) > 0 && strcmp(nome3, nome2)> 0){
+
+                strcpy(aux, nome3);
+                strcpy(nome3, nome1);
+                puts(nome2);
+                puts(nome1);
+                puts(aux);
+            } else{
+
+                system("pause");
+                return main();
+            }
+
+            //Opção 2 pra rodar
+
+            /*comp1 = strcmp(nome1, nome2);
             //comp2 = strcmp(nome2, nome3);
             comp3 = strcmp(nome1, nome3);
 
-            
 
-            /*if(comp1 > 0){
-                puts(nome2);
-            }else{
-                puts(nome1);
-            }
-
-            if(comp2 > 0){
-                puts(nome1);
-            } else{
-                puts(nome3);
-            }
-
-            if(comp3 > 0){
-                puts(nome1);
-            } else{
-                puts(nome3);
-            }*/
-
-            /*if(comp1 > 0){
+            if(comp1 > 0){
                 //strcpy(nome3, nome1);
                 puts(nome3);
                 puts(nome2);
